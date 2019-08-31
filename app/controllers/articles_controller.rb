929bddd1f98bf0cbf56aa01f470d49683349363d
github username <github email address>
2019-08-30 22:20:29 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,24 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    # raise params.inspect -> shows params hash in an error message in browser
+    # raise params.inspect
+
+    #Query the database for Article record that matches the :id passed to the route.
+     #Store query in instance variable
+    @article = Article.find(params[:id])
+   
+    #Update the values passed from the form. The update method takes a hash of the attributes for the model as its argument.
+    # Save changes to the database
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+   
+    #redirect user to the show page so they can see updated record.
+    redirect_to article_path(@article)
+  end
 end
